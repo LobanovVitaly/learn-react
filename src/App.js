d0230a0f45610fa57847e0310ad806d9ed544adb
LobanovVitaly <git config --global user.email fly.reason@yandex.rugit config --global user.email>
2023-11-03 14:56:39 +0400

    Add lazy-component (lazy-import + suspense in App component)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React  from 'react';
-import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import HeaderContainer from './components/Header/HeaderContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import News from "./components/News/News";
-import Music from "./components/Music/Music";
-import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import NavbarContainer from "./components/Navbar/NavbarContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import LoginPage from "./components/Login/Login";
+import {BrowserRouter, Routes, Route, Router, Switch} from "react-router-dom";
+import React, {Suspense, lazy} from 'react';
+import store from "./redux/redux-store";
 import {connect, Provider} from "react-redux";
+import './App.css';
 import withRouter from "./hoc/withRouter";
 import {compose} from "redux";
 import {initializedApp} from "./redux/app-reducer";
+import HeaderContainer from './components/Header/HeaderContainer';
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import NavbarContainer from "./components/Navbar/NavbarContainer";
+import UsersContainer from "./components/Users/UsersContainer";
+import LoginPage from "./components/Login/Login";
 import Preloader from "./components/common/Preloader/Preloader";
-import store from "./redux/redux-store";
+//import ProfileContainer from './components/Profile/ProfileContainer';
+const ProfileContainer = lazy(() => import('./components/Profile/ProfileContainer'));
+//import DialogsContainer from "./components/Dialogs/DialogsContainer";
+const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'));
 
 class App extends React.Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.initializedApp();
     }
+
     render() {
-        if(!this.props.initialized){
-            return <Preloader />
+        if (!this.props.initialized) {
+            return <Preloader/>
         }
         return (
             <div className="app-wrapper">
@@ -32,8 +35,16 @@ class App extends React.Component {
                 <div className="app-wrapper-content">
                     <Routes>
                         <Route path='/login' element={<LoginPage/>}/>
-                        <Route path='/dialogs/*' element={<DialogsContainer/>}/>
-                        <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
+                        <Route path='/dialogs/*' element={
+                            <Suspense fallback={<Preloader/>}>
+                                <DialogsContainer/>
+                            </Suspense>
+                        }/>
+                        <Route path='/profile/:userId?' element={
+                            <Suspense fallback={<Preloader/>}>
+                                <ProfileContainer/>
+                            </Suspense>
+                        }/>
                         <Route path='/news' element={<News/>}/>
                         <Route path='/music' element={<Music/>}/>
                         <Route path='/settings' element={<Settings/>}/>
@@ -64,12 +75,12 @@ let AppComponent = compose(
 
 const AppContainer = (props) => { // дополнительная обертка, чтобы использовать AppComponent вместе с Provider и BrowserRouter в app.text.js
     return <React.StrictMode>
-                <BrowserRouter>
-                    <Provider store={store}>
-                        <AppComponent />
-                    </Provider>
-                </BrowserRouter>
-            </React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <AppComponent/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
 }
 
 export default AppContainer;
